819f0b2ea6a7eb0bacb542b7a62bfba70155a6cb
AMMI <AMMI@AMMI-PC>
2018-10-06 11:29:04 +0530

    My First Commit

---
+++ Mevan_Project/src/main/java/Mevan_Project/Mevan_Project/App.java
@@ -0,0 +1,13 @@
+package Mevan_Project.Mevan_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Mevan_Project/src/test/java/Mevan_Project/Mevan_Project/Login.java
@@ -0,0 +1,19 @@
+package Mevan_Project.Mevan_Project;
+
+import org.testng.annotations.Test;
+
+public class Login 
+{
+	@Test
+	public void CreateAccount()
+	{
+		System.out.println("Create Account");
+	}
+	
+	@Test
+	public void UpdateAccount()
+	{
+		System.out.println("Update Account");
+	}
+
+}
